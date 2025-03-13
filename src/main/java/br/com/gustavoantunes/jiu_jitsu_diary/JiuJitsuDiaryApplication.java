22daf25d403cbe56c2a64bed335ea6dfea7e14ec
Gustavo Antunes <gustavo.silva@ztech.net>
2025-03-12 21:33:14 -0300

    First Commit

---
+++ src/main/java/br/com/gustavoantunes/jiu_jitsu_diary/JiuJitsuDiaryApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavoantunes.jiu_jitsu_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiuJitsuDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiuJitsuDiaryApplication.class, args);
+	}
+
+}
